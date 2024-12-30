b4552520953703c8b24070ecd32dd3a9f23fda60
imdavyking <yungdynamic53@gmail.com>
2024-12-30 21:44:58 +0100

    added check

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -7,16 +7,21 @@ const lifeSourceManager = new ethers.Contract(
   abi
 );
 
+const getContract = () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+  const signer = provider.getSigner();
+  const lifeSourceManager = new ethers.Contract(
+    "0x04A24B8894fAf25989d47B2DeF745Ed098258b16",
+    abi,
+    signer
+  );
+  return lifeSourceManager;
+};
+
 export const addPointService = async (weight) => {
   try {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-    const signer = provider.getSigner();
-    const lifeSourceManager = new ethers.Contract(
-      "0x04A24B8894fAf25989d47B2DeF745Ed098258b16",
-      abi,
-      signer
-    );
+    const lifeSourceManager = getContract();
     await lifeSourceManager.addPointFromWeight(Math.trunc(weight));
     return true;
   } catch (error) {
@@ -27,14 +32,7 @@ export const addPointService = async (weight) => {
 
 export const redeemCodeService = async (point) => {
   try {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-    const signer = provider.getSigner();
-    const lifeSourceManager = new ethers.Contract(
-      "0x04A24B8894fAf25989d47B2DeF745Ed098258b16",
-      abi,
-      signer
-    );
+    const lifeSourceManager = getContract();
     await lifeSourceManager.redeemCode(Math.trunc(point));
     return true;
   } catch (error) {
